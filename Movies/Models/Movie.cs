341459e6c3c13ebe0bc5f4d75fb1ab582ec2fd95
Emmaculate Kamau <ekamau@637capital.com>
2022-07-01 09:53:39 +0300

    Add model movie

---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        //A [DataType] attribute that specifies the type of data in the ReleaseDate
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
--- Movies/Program.cs
+++ Movies/Program.cs
@@ -23,3 +23,13 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+/*
+app.UseHttpsRedirection(); : Redirects HTTP requests to HTTPS.
+app.UseStaticFiles(); : Enables static files, such as HTML, CSS, images, and JavaScript to be served. For more information, see Static files in ASP.NET Core.
+app.UseRouting(); : Adds route matching to the middleware pipeline. For more information, see Routing in ASP.NET Core
+app.MapRazorPages();: Configures endpoint routing for Razor Pages.
+app.UseAuthorization(); : Authorizes a user to access secure resources. This app doesn't use authorization, therefore this line could be removed.
+app.Run(); : Runs the app.
+*/
